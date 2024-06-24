eb00dbe8210327f4bb7c5b10d4cb6fb29f25e685
niraj-wh <niraj@whitehatengineering.com>
2024-06-24 10:18:58 +0545

    Two Stacks using an array

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-import Stack.StackUsingLinkedList;
+import Stack.TwoStacks;
 
 class Main {
     public static void main(String[] args) {
-        var stack = new StackUsingLinkedList();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-        // System.out.println(stack.peek());
-        stack.print();
+        var twoStacks = new TwoStacks(5);
+        twoStacks.push1(1);
+        twoStacks.push1(2);
+        twoStacks.push2(3);
+        twoStacks.push2(4);
+        twoStacks.push2(5);
+        twoStacks.print();
+        twoStacks.pop1();
+        twoStacks.pop1();
+        twoStacks.pop2();
+        twoStacks.print();
     }
 }
