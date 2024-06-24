1ebba8cd985b2ba5d704e01fff9bbf29e2ff1ba3
niraj-wh <niraj@whitehatengineering.com>
2024-06-24 13:28:53 +0545

    Stack using a queue

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
-import Queues.ArrayQueue;
+import Queues.StackQueue;
 
 class Main {
     public static void main(String[] args) {
-
+        var queueUsingStack = new StackQueue();
+        queueUsingStack.add(1);
+        queueUsingStack.add(2);
+        queueUsingStack.add(3);
+        queueUsingStack.remove();
+        queueUsingStack.remove();
+        queueUsingStack.print();
     }
 }
