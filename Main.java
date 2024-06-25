eb1a5109c7c131f3f8c0faa6fe467f9c57e0fa67
niraj-wh <niraj@whitehatengineering.com>
2024-06-25 21:21:41 +0545

    Priority queue

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
-import Queues.StackQueue;
+import Queues.PriorityQueue;
 
 class Main {
     public static void main(String[] args) {
-        var queueUsingStack = new StackQueue();
-        queueUsingStack.add(1);
-        queueUsingStack.add(2);
-        queueUsingStack.add(3);
-        queueUsingStack.remove();
-        queueUsingStack.remove();
-        queueUsingStack.print();
+        var queue = new PriorityQueue(5);
+        queue.add(4);
+        queue.add(2);
+        queue.add(5);
+        queue.add(3);
+        queue.add(16);
+        queue.remove();
+        queue.remove();
+        queue.add(20);
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.add(4);
+        queue.add(2);
+        queue.add(5);
+        queue.add(3);
+        queue.add(16);
+        queue.print();
     }
 }
---
+++ Queues/PriorityQueue.java
@@ -0,0 +1,63 @@
+package Queues;
+
+import java.util.Arrays;
+
+public class PriorityQueue {
+    public int[] array;
+    public int currentIndex = -1;
+
+    public PriorityQueue(int size) {
+        this.array = new int[size];
+    }
+
+    public void add(int value) {
+        if (this.currentIndex == (this.array.length - 1)) {
+            throw new IllegalArgumentException("Out of bound.");
+        }
+        if (this.currentIndex == -1) {
+            this.currentIndex++;
+            this.array[this.currentIndex] = value;
+            return;
+        }
+        for (int i = this.currentIndex; i >= 0; i--) {
+            var item = this.array[i];
+            if (value < item) {
+                this.array[i + 1] = this.array[i];
+            } else {
+                this.array[i + 1] = value;
+                break;
+            }
+            if (i == 0) {
+                this.array[i] = value;
+            }
+        }
+        this.currentIndex++;
+    }
+
+    public int remove() {
+        if (this.currentIndex == -1) {
+            throw new IllegalArgumentException("No items to remove.");
+        }
+        int[] newArray = new int[this.array.length];
+        var i = 0;
+        while (i < this.currentIndex) {
+            newArray[i] = this.array[i + 1];
+            i++;
+        }
+        var value = this.array[0];
+        this.array = newArray;
+        this.currentIndex--;
+        return value;
+    }
+
+    public void print() {
+        int[] newArray = new int[this.currentIndex + 1];
+        var i = 0;
+        while (i <= this.currentIndex) {
+            newArray[i] = this.array[i];
+            i++;
+        }
+        System.out.println(Arrays.toString(newArray));
+    }
+
+}
