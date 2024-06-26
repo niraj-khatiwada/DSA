acb1472653a496637791458b6be1b6024d6ac7e5
niraj-wh <niraj@whitehatengineering.com>
2024-06-27 00:03:30 +0545

    Count unique pairs with difference k

--- Main.java
+++ Main.java
@@ -1,24 +1,9 @@
-import Hashmap.HashMap;
+import Hashmap.CountUniquePairsWithDiffK;
 
 class Main {
     public static void main(String[] args) {
-        var map = new HashMap(5);
-
-        map.put(1, "a");
-        map.put(2, "b");
-        map.put(6, "f");
-        System.out.println(map.size());
-        System.out.println(map.containsKey(1));
-        System.out.println(map.get(1));
-        map.put(1, "A");
-        System.out.println(map.get(1));
-        map.remove(1);
-        map.remove(6);
-        map.remove(100);
-        System.out.println(map.size());
-        System.out.println(map.containsKey(1));
-        System.out.println(map.get(1));
-
-        System.out.println(map);
+        var map = new CountUniquePairsWithDiffK();
+        int[] array = { 1, 7, 5, 9, 2, 12, 3 };
+        System.out.println(map.count(array, 2));
     }
 }
