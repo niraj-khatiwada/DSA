359cc1f51cf74ecf4771bd4fda0d555215631d8b
niraj-wh <niraj@whitehatengineering.com>
2024-06-26 23:03:20 +0545

    Creating hasmap using linkedlist chaining method

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
-import Hashmap.FirstMostRepeatedCharacter;
+import Hashmap.HashMap;
 
 class Main {
     public static void main(String[] args) {
-        var map = new FirstMostRepeatedCharacter();
-        System.out.println(map.find("banana"));
+        var map = new HashMap(5);
+
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(6, "f");
+        System.out.println(map.size());
+        System.out.println(map.containsKey(1));
+        System.out.println(map.get(1));
+        map.put(1, "A");
+        System.out.println(map.get(1));
+        map.remove(1);
+        map.remove(6);
+        map.remove(100);
+        System.out.println(map.size());
+        System.out.println(map.containsKey(1));
+        System.out.println(map.get(1));
+
+        System.out.println(map);
     }
 }
