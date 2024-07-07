b90741c0db515e73c5bf51a0ba632d2b9a821888
niraj-wh <niraj@whitehatengineering.com>
2024-07-07 15:30:00 +0545

    AVL trees rotations

--- Main.java
+++ Main.java
@@ -3,11 +3,20 @@ import Trees.AVLTree;
 class Main {
     public static void main(String[] args) {
         var avlTree = new AVLTree();
+        // avlTree.insert(5);
+        // avlTree.insert(4);
+        // avlTree.insert(7);
+        // avlTree.insert(9);
+        // avlTree.insert(3);
+        // avlTree.insert(11);
+        // avlTree.insert(1);
+        // avlTree.insert(2);
+        avlTree.insert(3);
         avlTree.insert(5);
         avlTree.insert(4);
+        avlTree.insert(8);
         avlTree.insert(7);
-        avlTree.insert(9);
-        avlTree.insert(3);
-        System.out.println(avlTree.height());
+        // avlTree.insert(4);
+        // System.out.println(avlTree.height());
     }
 }
--- Trees/AVLTree.java
+++ Trees/AVLTree.java
@@ -15,33 +15,100 @@ public class AVLTree {
     private Node root;
 
     public void insert(int value) {
-        if (this.root == null) {
-            var newNode = new Node(value);
-            this.root = newNode;
-            return;
-        }
-        _insert(this.root, value);
-
+        this.root = _insert(this.root, value);
     }
 
-    private void _insert(Node node, int value) {
+    private Node _insert(Node node, int value) {
         if (node == null) {
-            return;
+            return new Node(value);
         }
-        var newNode = new Node(value);
-        if (value < node.value && node.left == null) {
-            node.left = newNode;
-            node.height = this._height(node);
-            return;
+        if (value > node.value) {
+            node.right = _insert(node.right, value);
+        } else {
+            node.left = _insert(node.left, value);
         }
-        if (value > node.value && node.right == null) {
-            node.right = newNode;
-            node.height = this._height(node);
-            return;
-        }
-        node.height = this._height(node);
-        _insert(value < node.value ? node.left : node.right, value);
+        node.height = this._calculateHeight(node);
+        return this._balance(node);
+    }
 
+    private Node _balance(Node node) {
+        var rootBalanceFactor = this._findBalanceFactor(node);
+        if (rootBalanceFactor > 1) {
+            // Imbalance found on left side
+            System.out.printf("Imbalance found on left side at %s. Height = %s\n", node.value, node.height);
+            // Now, we need to detect if imbalance is L(Left Skewed), or LR(Left-Right).
+            var leftNodeBalanceFactor = this._findBalanceFactor(node.left);
+            // Left Skewed -> BF will be positive
+            if (leftNodeBalanceFactor > 0) {
+                System.out.printf("Left Skewed. Perform Right rotation at %s\n", node.value);
+                // Since we need to rotate right on both cases, we'll do at the end after
+                // checking if it is LR or not. See first return statement below.
+            } else {
+                // Left Right -> BF will be negative
+                System.out.printf(
+                        "Need Left-Right rotation. Perform Left rotation at %s and then Right rotation at %s\n",
+                        node.left.value, node.left.right.value);
+                node.left = this._rotateLeft(node.left);
+            }
+            return this._rotateRight(node);
+
+        }
+        if (rootBalanceFactor < -1) {
+            // Imbalance found on right side
+            System.out.printf("Imbalance found on right side at %s. Height = %s\n", node.value, node.height);
+            // Now we need to detect if imbalance is R(Right Skewed) or RL(Right-Left)
+            var rightNodeBalanceFactor = this._findBalanceFactor(node.right);
+            // Right Skewed = BF negative
+            if (rightNodeBalanceFactor < 0) {
+                System.out.printf("Right Skewed. Perform Left rotation at %s\n", node.value);
+                // Since we need to rotate left on both cases, we'll do at the end after
+                // checking if it is RL or not. See first return statement below.
+            } else {
+                // Right Left = BF positive
+                System.out.printf(
+                        "Need Right-Left rotation. Perform Right rotation at %s and then Left rotation at %s\n",
+                        node.right.value, node.right.left.value);
+                node.right = this._rotateRight(node.right);
+            }
+            return this._rotateLeft(node);
+        }
+        return node;
+    }
+
+    public Node _rotateRight(Node node) {
+        var oldRoot = node;
+        node = node.left;
+        oldRoot.left = null;
+        if (node.right == null) {
+            node.right = oldRoot;
+        } else {
+            var rightSubTree = node.right;
+            node.right = oldRoot;
+            node.right.left = rightSubTree;
+        }
+        node.right.height = this._calculateHeight(node.right); // old root
+        node.height = this._calculateHeight(node); // new root
+        return node;
+    }
+
+    public Node _rotateLeft(Node node) {
+        var oldRoot = node;
+        node = node.right;
+        oldRoot.right = null;
+        if (node.left == null) {
+            node.left = oldRoot;
+        } else {
+            var leftSubTree = node.left;
+            node.left = oldRoot;
+            node.left.right = leftSubTree;
+        }
+        node.left.height = this._calculateHeight(node.left); // old root
+        node.height = this._calculateHeight(node); // new root
+        return node;
+    }
+
+    public int _findBalanceFactor(Node node) {
+        return this._height(node.left) - this._height(node.right);
     }
 
     public int height() {
@@ -49,14 +116,11 @@ public class AVLTree {
     }
 
     private int _height(Node node) {
-        if (node == null) {
-            return -1;
-        }
-        return 1 + Math.max(_height(node.left), _height(node.right));
+        return node == null ? -1 : node.height;
     }
 
-    private boolean _isLeafNode(Node node) {
-        return node.left == null && node.right == null;
+    private int _calculateHeight(Node node) {
+        return 1 + Math.max(this._height(node.left), this._height(node.right));
     }
 
 }
