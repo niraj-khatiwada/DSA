be4cbe60cbb7a582e23781ab9a0ef5fbe9fd2c39
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-16 11:44:23 +0545

    Some general problems

--- Main.java
+++ Main.java
@@ -1,19 +1,4 @@
-import Graphs.Graph;
 
 class Main {
-    public static void main(String[] args) {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        // graph.addEdge("C", "A");
-        graph.addEdge("D", "A");
-
-        System.out.println(graph.hasCycle());
-    }
 
 }
