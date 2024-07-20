e1da2386060266dead200d8a87d63cd83ff62ec6
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-20 17:02:01 +0545

    Counting Sort Algorithm

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import Sorting.QuickSort;
+import Sorting.CountingSort;
 
 import java.util.Arrays;
 
 class Main {
 
     public static void main(String[] args) {
-        var sort = new QuickSort();
-        var array = new int[] { 15, 6, 3, 1, 22, 10, 13 };
+        var sort = new CountingSort();
+        var array = new int[] { 5, 4, 2, 3, 1, 1, 5 };
         sort.sort(array);
         System.out.println(Arrays.toString(array));
 
