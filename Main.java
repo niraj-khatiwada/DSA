4bf04ae31d4cae9704a54198ee348b7300d1861d
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-20 12:51:37 +0545

    Merge sort

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import Sorting.InsertionSort;
+import Sorting.MergeSort;
 
 import java.util.Arrays;
 
 class Main {
 
     public static void main(String[] args) {
-        var sort = new InsertionSort();
-        var array = new int[] { 8, 3, 1, 2, 4 };
+        var sort = new MergeSort();
+        var array = new int[] { 4, 8, 1, 2, 3 };
         sort.sort(array);
         System.out.println(Arrays.toString(array));
 
