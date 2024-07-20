5283bee9c05d5da622e88e7d86b1de687d89a1a0
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-20 16:06:26 +0545

    Quick sort

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import Sorting.MergeSort;
+import Sorting.QuickSort;
 
 import java.util.Arrays;
 
 class Main {
 
     public static void main(String[] args) {
-        var sort = new MergeSort();
-        var array = new int[] { 4, 8, 1, 2, 3 };
+        var sort = new QuickSort();
+        var array = new int[] { 15, 6, 3, 1, 22, 10, 13 };
         sort.sort(array);
         System.out.println(Arrays.toString(array));
 
---
+++ Sorting/QuickSort.java
@@ -0,0 +1,52 @@
+package Sorting;
+
+import java.util.Stack;
+
+public class QuickSort {
+    public void sort(int[] array) {
+        var stack = new Stack<Integer>();
+        for (var i = 0; i < array.length; i++) {
+            stack.add(array[i]);
+        }
+        // We use stack to start from last element
+        while (!stack.isEmpty()) {
+            var nextItem = stack.pop();
+            var pivotIndex = -1;
+            // The original array will be changed so we need to find the exact index of last
+            // popped value. That will be the pivot index. This makes sure we use every
+            // element as a pivot once.
+            for (var i = 0; i < array.length; i++) {
+                if (nextItem == array[i]) {
+                    pivotIndex = i;
+                }
+            }
+            // Now we do comparison between index 0 and pivotIndex for each element.
+            // We shift the element one element to left if array[i] > array[pivot]
+            var i = 0;
+            while (true) {
+                if (i == pivotIndex) {
+                    break;
+                }
+                var target = array[i];
+                // If target value is greater than pivot value, shift each values one step to
+                // the left and replace pivot with target value. Perform this for every value
+                // from stack.
+                if (target > array[pivotIndex]) {
+                    var currentIndex = pivotIndex;
+                    var previousRef = array[currentIndex];
+                    while (currentIndex > i) {
+                        var tempRef = array[currentIndex - 1];
+                        array[currentIndex - 1] = previousRef;
+                        previousRef = tempRef;
+                        currentIndex--;
+                    }
+                    array[pivotIndex] = target;
+                    pivotIndex--;
+                    i = 0;
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+}
