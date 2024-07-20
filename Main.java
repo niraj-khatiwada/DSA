797401b70cfeecdee0853a55bfdfcbcd51890aaa
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-20 21:33:39 +0545

    Binary search using iteration and recursion

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-import Search.LinearSearch;
+import Search.BinarySearch;
 
 class Main {
 
     public static void main(String[] args) {
-        var search = new LinearSearch();
+        var search = new BinarySearch();
 
-        System.out.println(search.find(new int[] { 1, 2, 3 }, -3));
+        System.out.println(search.find(new int[] { 3, 6, 7, 8, 11 }, 8));
+        System.out.println(search.findUsingRecursion(new int[] { 3, 6, 7, 8, 11 }, 8));
     }
 
 }
---
+++ Search/BinarySearch.java
@@ -0,0 +1,58 @@
+package Search;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public int find(int[] array, int value) {
+        Arrays.sort(array);
+        var leftIndex = 0;
+        var rightIndex = array.length - 1;
+
+        while (true) {
+            int middleIndex = (leftIndex + rightIndex) / 2;
+            var middleValue = array[middleIndex];
+            if (middleValue == value) {
+                return middleIndex;
+            }
+            if ((leftIndex + 1) == rightIndex) {
+                if (array[rightIndex] == value) {
+                    return rightIndex;
+                } else {
+                    // Left also didn't match, right also didn't match. So, no match.
+                    break;
+                }
+            }
+            if (value > middleValue) {
+                leftIndex = middleIndex;
+            } else {
+                rightIndex = middleIndex;
+            }
+        }
+        return -1;
+    }
+
+    public int findUsingRecursion(int[] array, int value) {
+        return this._findUsingRecursion(array, value, 0, array.length - 1);
+    }
+
+    private int _findUsingRecursion(int[] array, int value, int leftIndex, int rightIndex) {
+        int middleIndex = (leftIndex + rightIndex) / 2;
+        var middleItem = array[middleIndex];
+        if (middleItem == value) {
+            return middleIndex;
+        }
+        if ((leftIndex + 1) == rightIndex) {
+            if (array[rightIndex] == value) {
+                return rightIndex;
+            }
+            return -1;
+        }
+        if (value < middleItem) {
+            rightIndex = middleIndex;
+        } else {
+            leftIndex = middleIndex;
+        }
+        return _findUsingRecursion(array, value, leftIndex, rightIndex);
+
+    }
+}
