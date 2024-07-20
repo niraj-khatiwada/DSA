1bfbe47a4c6afe58fe5ebf18767ef83204dbedf3
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-20 11:11:24 +0545

    Insertion Sort

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import Sorting.BubbleSort;
+import Sorting.InsertionSort;
 
 import java.util.Arrays;
 
 class Main {
 
     public static void main(String[] args) {
-        var sort = new BubbleSort();
-        var array = new int[] { 1, 2, 4, 3 };
+        var sort = new InsertionSort();
+        var array = new int[] { 8, 3, 1, 2, 4 };
         sort.sort(array);
         System.out.println(Arrays.toString(array));
 
