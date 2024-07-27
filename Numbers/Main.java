f04c1c161ba7b894bef80900f4df00d0befd1226
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-27 13:53:23 +0545

    Natural Numbers def

--- Numbers/Main.java
+++ Numbers/Main.java
@@ -24,6 +24,7 @@ public class Main {
         return count == 2;
     }
 
+    // Natural numbers mean 1,2,3,4...Math.MAX_VALUE
     public static int sumOfNaturalNumbers(int num) {
         return Main._sumOfNaturalNumbers(num);
     }
