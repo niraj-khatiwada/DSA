a102031e7e603b905cf47cdbd980339d1ea59358
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-14 21:07:30 +0545

    Selection Sort

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-import Sorting.BubbleSort;
+import Sorting.SelectionSort;
 
 class Main {
 
     public static void main(String[] args) {
-        var sort = new BubbleSort();
+        var sort = new SelectionSort();
         var array = new int[] { 6, 5, 4, 3, 1 };
         sort.sort(array);
         System.out.println(Arrays.toString(array));
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class SelectionSort {
+    public void sort(int[] array) {
+        var p = 0;
+        for (var i = 0; i < array.length; i++) {
+            var minIndex = p;
+            for (var j = p; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            var ref = array[p];
+            array[p] = array[minIndex];
+            array[minIndex] = ref;
+            p++;
+        }
+    }
+}
