a95d458c55f5fb81489a52d430a2a2d90e1bde06
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-17 16:47:18 +0545

    Valid sudoku

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 import java.util.*;
 
-import Backtracking.GridWays;
+import Backtracking.ValidSudoku;
 
 class Main {
 
     public static void main(String[] args) {
-        var backtracking = new GridWays();
-        System.out.println(backtracking.findNumberOfWays(3, 3));
-        System.out.println(backtracking.findNumberOfWaysUsingMath(3, 3));
+        var backtracking = new ValidSudoku();
+        System.out.println(backtracking.validSudoku(new int[][] { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
+                { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
+                { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
+                { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
+                { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
+                { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
+                { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
+                { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
+                { 8, 2, 7, 0, 0, 9, 0, 1, 3 },
+        }));
     }
 
 }
