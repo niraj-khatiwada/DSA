9c551cab9d0499ed87ffd823b8515e067e9b7cdc
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-17 18:51:45 +0545

    Rat in Maze

---
+++ Backtracking/RatInMaze.java
@@ -0,0 +1,34 @@
+package Backtracking;
+
+import java.util.*;
+
+public class RatInMaze {
+    public void findPossiblePath(int[][] maze) {
+        var visited = new TreeSet<int[]>((a, b) -> Arrays.compare(a, b));
+        this._findPossiblePath(maze, 0, 0, visited, "");
+    }
+
+    private void _findPossiblePath(int[][] maze, int x, int y, TreeSet<int[]> visited, String path) {
+        if (x < 0 || x >= maze.length || y < 0 || y >= maze[0].length || maze[x][y] == 0
+                || visited.contains(new int[] { x, y })) {
+            return;
+        }
+
+        if (x == (maze.length - 1) && y == (maze[0].length - 1)) {
+            System.out.println(path.toString());
+            return;
+        }
+
+        var coord = new int[] { x, y };
+        visited.add(coord);
+
+        _findPossiblePath(maze, x, y + 1, visited, path + "R");
+        _findPossiblePath(maze, x + 1, y, visited, path + "D");
+        _findPossiblePath(maze, x, y - 1, visited, path + "L");
+        _findPossiblePath(maze, x - 1, y, visited, path + "U");
+
+        visited.remove(coord);
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,21 +1,15 @@
 import java.util.*;
 
-import Backtracking.ValidSudoku;
+import Backtracking.RatInMaze;
 
 class Main {
 
     public static void main(String[] args) {
-        var backtracking = new ValidSudoku();
-        System.out.println(backtracking.validSudoku(new int[][] { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
-                { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
-                { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
-                { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
-                { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
-                { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
-                { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
-                { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
-                { 8, 2, 7, 0, 0, 9, 0, 1, 3 },
-        }));
+        var backtracking = new RatInMaze();
+        backtracking.findPossiblePath(new int[][] { { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+        });
     }
 
 }
