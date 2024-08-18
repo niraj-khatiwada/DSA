85e926726ffba092290561d3b2a50a2938998e47
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-18 22:24:18 +0545

    Permutations & Combinations

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 import java.util.*;
 
-import Backtracking.CombinationTargetSum;
+import Backtracking.PermutationAndCombination;
 
 class Main {
 
     public static void main(String[] args) {
-        var backtracking = new CombinationTargetSum();
-        // backtracking.solveKT();
-        System.out.println(backtracking.combinationSum(new int[] { 2, 3, 6, 7 }, 7));
+        var backtracking = new PermutationAndCombination();
 
     }
 
