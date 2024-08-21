f9be298c909b95ec4d70f10ac8b7ce4015aac2e0
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-21 22:02:19 +0545

    Knapsack 0-1

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 
-import DynamicProgramming.ClimbingStairs;
+import DynamicProgramming.KnapSack01;
 
 class Main {
 
     public static void main(String[] args) {
-        var f = new ClimbingStairs();
-        System.out.println(f.countPossibilities(5));
+        var f = new KnapSack01();
+        System.out.println(f.maxProfit(new int[] { 15, 14, 10, 45, 30 }, new int[] { 2, 5, 1, 3, 4 }, 7));
 
     }
 
