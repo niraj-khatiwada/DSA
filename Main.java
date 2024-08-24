10202226c5ed2a99ab487b4098a0b1fff3ef603d
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-24 17:13:48 +0545

    Fractional Knapsack

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
-import Greedy.ActivitySelection;
+import Greedy.FractionalKnapsack;
 
 class Main {
 
     public static void main(String[] args) {
-        var greedy = new ActivitySelection();
-        System.out.println(greedy.maxNumberOfJobs(new int[] { 1, 3, 0, 5, 8, 5 },
-                new int[] { 2, 4, 6, 7, 9, 9 }));
+        var greedy = new FractionalKnapsack();
+        System.out.println(greedy.maxProfit(
+                new int[][] { { 120, 30 }, { 100, 20 }, { 62, 10 } },
+                50));
+        System.out.println((double) 10 / 3);
 
     }
 }
