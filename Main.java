da4534dfd6099620071f9f2be5e80f1c0956ee2a
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-29 00:04:38 +0545

    closest point for a given condition binarys earch

--- Main.java
+++ Main.java
@@ -7,6 +7,24 @@ class Main {
     public static void main(String[] args) {
         var greedy = new ChocolaProblem();
         System.out.println(greedy.minimalCost(new int[] { 4, 1, 2 }, new int[] { 2, 1, 3, 1, 4 }));
+        System.out.println(_search(new int[][] { { 1, 3 }, { 2, 4 }, { 3, 5 }, { 3, 6 } }, 0));
+    }
+
+    private static int _search(int[][] matrix, int i) {
+        var l = i + 1;
+        var r = matrix.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (matrix[m][0] >= matrix[i][1]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+            if (l == r) {
+                return r;
+            }
+        }
+        return -1;
     }
 
 }
