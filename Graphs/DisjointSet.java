729737b7cdb4a58be6db27855c6cf1f96667a345
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-29 22:20:46 +0545

    Disjoint Set

--- Graphs/DisjointSet.java
+++ Graphs/DisjointSet.java
@@ -3,52 +3,41 @@ package Graphs;
 import java.util.*;
 
 public class DisjointSet {
-    public List<Integer> parent;
-    public List<Integer> rank;
+    public int[] parent;
+    public int[] rank;
 
     public DisjointSet(int size) {
-        // If size is 7, you can only pass values between 1 & 7.
-        this.parent = new ArrayList<Integer>();
-        this.rank = new ArrayList<Integer>();
+        this.parent = new int[size + 1];
+        this.rank = new int[size + 1];
         for (var i = 0; i <= size; i++) {
-            parent.add(i);
-            rank.add(0);
+            parent[i] = i;
+            rank[i] = 0;
         }
     }
 
     public int find(int n) {
-        var p = parent.get(n);
-        if (p == n) {
+        var p = parent[n];
+        if (n == parent[n]) {
             return n;
         }
-        return find(p);
+        return parent[p] = find(p);
     }
 
     public int union(int a, int b) {
-        var rankA = rank.get(a);
-        var rankB = rank.get(b);
-
         var parentA = find(a);
         var parentB = find(b);
 
+        var rankA = rank[a];
+        var rankB = rank[b];
+
         if (rankA >= rankB) {
-            parent.set(parentB, parentA);
+            parent[parentB] = parentA;
             if (rankA == rankB) {
-                rank.set(parentA, rankB + rankA + 1);
+                rank[parentA]++;
             }
             return parentA;
         }
-        parent.set(parentA, parentB);
+        parent[parentA] = parentB;
         return parentB;
-    }
-
-    public int numberOfDisjointSets() {
-        var c = 0;
-        for (var i = 0; i < parent.size(); i++) {
-            if (parent.get(i) == i) {
-                c++;
-            }
-        }
-        return c - 1; // -1 for 0
     }
 }
--- Main.java
+++ Main.java
@@ -5,16 +5,12 @@ import Graphs.DisjointSet;
 class Main {
 
     public static void main(String[] args) {
-        var ds = new DisjointSet(4);
-        System.out.println(ds.find(1));
-        ds.union(1, 2);
-        System.out.println(ds.find(2));
-        ds.union(3, 4);
-        System.out.println(ds.find(3));
-
-        System.out.println(ds.parent);
-        System.out.println(ds.rank);
-        System.out.println(ds.numberOfDisjointSets());
+        var ds = new DisjointSet(5);
+        ds.union(0, 3);
+        ds.union(1, 3);
+        ds.union(1, 4);
+        ds.union(1, 5);
+        ds.union(2, 5);
     }
 
 }
