2a0800517039d520e9811857c3a2447c87bcc7ce
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-05 14:50:34 +0545

    Dice Roll Combinations

--- Main.java
+++ Main.java
@@ -1,24 +1,12 @@
 import java.util.*;
 
-import Graph.DirectedGraph;
+import Recursion.DiceRoll;
 
 class Main {
 
     public static void main(String[] args) {
-        var graph = new DirectedGraph();
-        graph.addNode(1);
-        graph.addNode(2);
-        graph.addNode(3);
-        graph.addNode(4);
-        graph.addNode(5);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 4);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-
-        // graph.preOrderDfs(1);
-        System.out.println(graph.pathExists(1, 5));
+        var dice = new DiceRoll();
+        dice.firstDiceCombinations(2, 5);
     }
 
 }
