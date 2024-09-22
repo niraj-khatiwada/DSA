611c37396da16e6bbce90e185912ec12db66bd1f
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-22 21:02:43 +0545

    String compression

--- Main.java
+++ Main.java
@@ -6,8 +6,9 @@ class Main {
 
     public static void main(String[] args) {
         var str = new StringCompression();
-        System.out.println(str.compress("aaabbcdddaaa"));
-        System.out.println(str.decompress("a3b2cd3a3"));
+        System.out.println(str.compress("aaaaaaaaaabbc"));
+        System.out.println(str.decompress(str.compress("aaaaaaaaaabbc")));
+
     }
 
 }
--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -2,6 +2,7 @@ package Strings;
 
 public class StringCompression {
     // aaabbc
+    // aaaaaaaaaabbc. Remember count can be greater than 9 as well.
     public String compress(String str) {
         var rs = new StringBuilder();
         var last = '.';
@@ -9,7 +10,11 @@ public class StringCompression {
         for (var ch : str.toCharArray()) {
             if (ch == last) {
                 if (count > 1) {
-                    rs.deleteCharAt(rs.length() - 1);
+                    var c = count;
+                    while (c != 0) {
+                        rs.deleteCharAt(rs.length() - 1);
+                        c = c / 10;
+                    }
                 }
                 rs.append((count + 1) + "");
                 count++;
@@ -22,13 +27,20 @@ public class StringCompression {
         return rs.toString();
     }
 
+    // aaaaaaaaaabbc. Remember count can be greater than 9 as well.
     public String decompress(String str) {
         var rs = new StringBuilder();
         var last = '.';
-        for (var ch : str.toCharArray()) {
-            if (ch > 48 && ch < 57) {
-                var p = ch - 48 - 1;
-                while (p > 0) {
+        var i = 0;
+        while (i < str.length()) {
+            var ch = str.charAt(i);
+            if (ch > 48 && ch < 58) {
+                var p = ch - 48;
+                while ((i + 1) < str.length() && str.charAt(i + 1) >= 48 && str.charAt(i + 1) < 58) {
+                    p = (p * 10) + (str.charAt(i + 1) - 48);
+                    i++;
+                }
+                while (p > 1) {
                     rs.append(last);
                     p--;
                 }
@@ -36,6 +48,7 @@ public class StringCompression {
                 rs.append(ch);
                 last = ch;
             }
+            i++;
         }
         return rs.toString();
     }
